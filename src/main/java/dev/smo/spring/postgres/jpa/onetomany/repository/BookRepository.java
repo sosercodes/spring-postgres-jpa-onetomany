efbc0da57d78fc35d7e31b149cad89e29322a29c
Simon Oser <simon.oser@gmail.com>
2024-07-29 13:59:43 +0200

    Add BookRepository and test class

---
+++ src/main/java/dev/smo/spring/postgres/jpa/onetomany/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package dev.smo.spring.postgres.jpa.onetomany.repository;
+
+import dev.smo.spring.postgres.jpa.onetomany.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
+
+}
---
+++ src/test/java/dev/smo/spring/postgres/jpa/onetomany/repository/BookRepositoryTest.java
@@ -0,0 +1,64 @@
+package dev.smo.spring.postgres.jpa.onetomany.repository;
+
+import dev.smo.spring.postgres.jpa.onetomany.TestcontainersConfiguration;
+import dev.smo.spring.postgres.jpa.onetomany.model.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class BookRepositoryTest {
+
+    @Autowired
+    PostgreSQLContainer<?> postgres;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void save() {
+        Book b1 = Book.builder()
+                .title("Book 1")
+                .price(BigDecimal.valueOf(39.99))
+                .publishDate(LocalDate.of(2023, 5, 5))
+                .build();
+        Book b1saved = bookRepository.save(b1);
+        assertNotNull(b1saved);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void findByTitle() {
+        Book b1 = Book.builder()
+                .title("BookWithTitle")
+                .price(BigDecimal.valueOf(39.99))
+                .publishDate(LocalDate.of(2023, 5, 5))
+                .build();
+        bookRepository.save(b1);
+        List<Book> books = bookRepository.findByTitle("BookWithTitle");
+
+        assertNotNull(books);
+        assertFalse(books.isEmpty());
+        assertEquals("BookWithTitle", books.getFirst().getTitle());
+    }
+}
